4e1f0e33d5f0799e8449204d03afc02f16914a54
Grant <438344441@qq.com>
2018-08-02 13:51:33 +0800

    verson1

---
+++ t1.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoSum(self, nums, target):
+        n = len(nums)
+        for i in range(n):
+            for j in range(i+1,n):
+                if target - nums[i] == nums[j]:
+                    return i,j
+                else:
+                    continue
+
