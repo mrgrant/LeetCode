77760a84e4ffc00e24f7bf680b455be79ef83549
Grant <438344441@qq.com>
2018-08-09 14:01:35 +0800

    ver2

--- 66_plus_one.py
+++ 66_plus_one.py
@@ -1,17 +1,19 @@
 class Solution:
     def plusOne(self, digits):
-        if len(digits) == 0:
-            return [1]
+        res = []
+        if digits[-1] != 9:
+            digits [-1] = digits[-1] + 1
+            res = digits
         else:
-            s = 0
-            for i in digits:
-                s = s*10 + i
-            s = s + 1
-            l=[]
-            while s != 0:
-                l.append(s%10)
-                s = s // 10   
-            return l[::-1]
+            num = 0
+            digits.reverse()
+            for i in range(len(digits)):
+                num = num + digits[i]*(10**i)
+            num = str(num+1)
+            for j in num:
+                res.append(int(j))
+        return res
+
 
 
 if __name__ == '__main__':
