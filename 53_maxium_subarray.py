4a3efb764420f052b88754f853b512157c4333ea
Grant <mrgrantcy@gmail.com>
2018-08-23 15:38:33 +0800

    8.23

---
+++ 53_maxium_subarray.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        length = len(nums)
+        for i in range(1,length):
+            maxsub = max(nums[i]+nums[i-1], nums[i])
+            nums[i] = maxsub
+        return max(nums)
+
+
+if __name__ == '__main__':
+    a = [-2,1,-3,4,-1,2,1,-5,4]
+    print(Solution().maxSubArray(a))
